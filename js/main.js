1a8ac3ea2b78bdbe13bcf6da04845c12cfe6dae7
diana <dskripka005@gmail.com>
2023-11-22 18:07:29 +0200

    two

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const galleryE1 = document.querySelectorAll('.image') ;
+const imgContainer = document.querySelector('.full-image-container') ;
+const fullImage = document.querySelector('.full-image') ;
+console.log(galleryE1);
+
+let currentIndex = 0;
+galleryE1.forEach( (img, index) => { console.log(img);
+img.addEventListener('click', () => {console.log(index); }  )
+currentIndex = index;
+})
+
+function fullScreenImg(index) {
+    fullImage.src = galleryE1[index]
+}
+
+
+
+
+
+//2
