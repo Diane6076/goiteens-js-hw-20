6c95b447d9a24cd369d823d1ba72bb116f6108a2
diana <dskripka005@gmail.com>
2023-11-24 19:58:16 +0200

    fixreddtwo

--- js/main.js
+++ js/main.js
@@ -19,3 +19,31 @@ function fullScreenImg(index) {
 
 //2
 
+function createBoxes() {
+    const amount = document.getElementById('boxNumber').value;
+    const boxesContainer = document.getElementById('boxes');
+    boxesContainer.innerHTML = '';
+    
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.backgroundColor = getRandomColor();
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      boxesContainer.appendChild(box);
+    }
+    }
+    
+    function destroyBoxes() {
+    const boxesContainer = document.getElementById('boxes');
+    boxesContainer.innerHTML = '';
+    }
+    
+    function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    }
